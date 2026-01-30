493941d28d3b5b969f8cc1fb45256ad5d7904e48
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-01-31 05:55:21 +1000

    Tags system

---
+++ AgarthaLib/Tags/TagWhitelist.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AgarthaLib.Tags
+{
+    [Serializable] public class TagWhitelist<T> where T : IComparable
+    {
+        public List<T> Whitelist = new();
+        public List<T> Blacklist = new();
+
+        public bool IsWhitelistPass(T t)
+            => Whitelist.Any(q => t.Equals(q));
+
+        public bool IsWhitelistPass(List<T> l)
+            => Whitelist.Any(q => l.Contains(q));
+
+        public bool IsBlacklistPass(T t)
+            => Blacklist.Any(q => t.Equals(q));
+
+        public bool IsBlacklistPass(List<T> l)
+            => Blacklist.Any(q => l.Contains(q));
+
+        public bool Pass(T t)
+        {
+            if (Whitelist.Count > 0) return IsWhitelistPass(t);
+            if (Blacklist.Count > 0) return IsBlacklistPass(t);
+            return true;
+        }
+
+        public bool Pass(List<T> l)
+        {
+            if (Whitelist.Count > 0) return IsWhitelistPass(l);
+            if (Blacklist.Count > 0) return IsBlacklistPass(l);
+            return true;
+        }
+    }
+}
