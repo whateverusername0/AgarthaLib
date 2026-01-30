42d9322f0560e3359a90b2a86edadf2a5984a512
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-01-31 05:55:28 +1000

    Directional sprites

---
+++ AgarthaLib/Sprites/Direction/DirectionalSpriteRenderer.cs
@@ -0,0 +1,34 @@
+﻿using AgarthaLib.MonoBehavior;
+using UnityEngine;
+
+namespace AgarthaLib.Sprites.Direction
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class DirectionalSpriteRenderer : AgarthanBehaviour
+    {
+        private SpriteRenderer _sr;
+        public DirectionalSprite Sprite;
+        public Transform Pivot;
+
+        private void Start()
+        {
+            _sr = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            if (Pivot == null) return;
+            var rot = Mathf.FloorToInt(Pivot.eulerAngles.z / 90);
+            this.transform.rotation = Quaternion.identity;
+
+            if (Sprite == null) return;
+            switch (rot)
+            {
+                case 0: default: _sr.sprite = Sprite.Up; break;
+                case 1: _sr.sprite = Sprite.Right; break;
+                case 2: _sr.sprite = Sprite.Down; break;
+                case 3: _sr.sprite = Sprite.Left; break;
+            }
+        }
+    }
+}
