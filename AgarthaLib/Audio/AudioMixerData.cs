37159673b435357744fdd8fec5ea2acfdbf8bc2d
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-02-02 04:37:46 +1000

    minimal audio system

---
+++ AgarthaLib/Audio/AudioMixerData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace AgarthaLib.Audio
+{
+    [CreateAssetMenu(menuName = "AgarthaLib / Audio / Audio Mixer Data")]
+    public class AudioMixerData : ScriptableObject
+    {
+        public AudioMixerGroup Group;
+        [Range(0f, 1f)] public float Volume = 1f;
+    }
+}
---
+++ AgarthaLib/Audio/AudioSourceController.cs
@@ -0,0 +1,18 @@
+﻿using AgarthaLib.Attributes;
+using AgarthaLib.MonoBehavior;
+using UnityEngine;
+
+namespace AgarthaLib.Audio
+{
+    public class AudioSourceController : AgarthanBehaviour
+    {
+        [ValidateNull] public AudioSource Source;
+        public AudioMixerData Data;
+
+        private void Update()
+        {
+            if (Data == null) return;
+            Source.volume = Data.Volume;
+        }
+    }
+}
