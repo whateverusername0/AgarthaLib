f2ed40e47dac497cf0e827f687537e888d63dc5a
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-02-27 02:17:19 +1000

    fuck!

--- AgarthaLib/Collision/Conditions/CollisionTagCondition.cs
+++ AgarthaLib/Collision/Conditions/CollisionTagCondition.cs
@@ -1,6 +1,9 @@
-﻿using AgarthaLib.MonoBehavior;
+﻿using AgarthaLib.Extensions;
+using AgarthaLib.MonoBehavior;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 namespace AgarthaLib.Collision.Conditions
 {
@@ -13,6 +16,7 @@ namespace AgarthaLib.Collision.Conditions
         {
             base.Start();
             SubscribeEvent<BeforeCollisionEnterEvent>(OnBeforeCollisionEnter);
+            SubscribeEvent<BeforeCollision2DEnterEvent>(OnBeforeCollision2DEnter);
         }
 
         private void OnBeforeCollisionEnter(object invoker, ref BeforeCollisionEnterEvent args)
@@ -20,8 +24,18 @@ namespace AgarthaLib.Collision.Conditions
             if (args.Cancelled) return;
 
             var target = args.GameObject;
-            var allowed = !Whitelist.Any(q => target.CompareTag(q));
-            var disallowed = Blacklist.Any(q => target.CompareTag(q));
+            var allowed = Whitelist.IsValid() && !Whitelist.Any(q => target.CompareTag(q));
+            var disallowed = Blacklist.IsValid() && Blacklist.Any(q => target.CompareTag(q));
+            args.Cancelled = allowed || disallowed;
+        }
+
+        private void OnBeforeCollision2DEnter(GameObject invoker, ref BeforeCollision2DEnterEvent args)
+        {
+            if (args.Cancelled) return;
+
+            var target = args.GameObject;
+            var allowed = Whitelist.IsValid() && !Whitelist.Any(q => target.CompareTag(q));
+            var disallowed = Blacklist.IsValid() && Blacklist.Any(q => target.CompareTag(q));
             args.Cancelled = allowed || disallowed;
         }
     }
