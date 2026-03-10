7bb511e966120d82218a626f6d032166c73c74c3
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-03-10 23:06:38 +1000

    Made EventBase classes abstract.

--- AgarthaLib/EventSystem/EventBase.cs
+++ AgarthaLib/EventSystem/EventBase.cs
@@ -2,14 +2,14 @@
 
 namespace AgarthaLib.EventSystem
 {
-    public class EventBase { }
+    public abstract class EventBase { }
 
-    public class CancellableEventBase : EventBase
+    public abstract class CancellableEventBase : EventBase
     {
         public bool Cancelled { get; set; }
     }
 
-    public class RelayedEvent<T> where T : class
+    public class RelayedEvent<T> where T : notnull
     {
         public GameObject Source;
         public T Args { get; set; }
@@ -18,6 +18,16 @@ namespace AgarthaLib.EventSystem
         {
             Source = invoker;
             Args = args;
+        }
+    }
+
+    public abstract class PropertyChangedEvent<T> where T : notnull
+    {
+        public T OldValue, NewValue;
+        public PropertyChangedEvent(T oldValue, T newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
         }
     }
 }
