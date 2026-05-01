cb862aa73ee6aa804e6afed17280d9b97d4f7abe
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-05-02 03:05:43 +1000

    fix portalawarephysics

--- AgarthaLib/Goodies/Portals/PortalAwarePhysics.cs
+++ AgarthaLib/Goodies/Portals/PortalAwarePhysics.cs
@@ -7,14 +7,13 @@ namespace AgarthaLib.Goodies.Portals
 {
     public static class PortalAwarePhysics
     {
-        public static bool RaycastPortalAware(Vector3 position, Vector3 direction, float distance, out RaycastHit[] result, int mask = -1)
+        public static bool RaycastPortalAware(Vector3 position, Vector3 direction, float distance, out List<RaycastHit> result, int mask = -1)
         {
-            var hits = new List<RaycastHit>();
-            result = hits.ToArray();
-            var rc = UnityEngine.Physics.RaycastAll(position, direction, distance, mask);
+            result = new();
+            var rc = Physics.RaycastAll(position, direction, distance, mask);
             if (rc.Length == 0) return false;
 
-            foreach (var hit in result)
+            foreach (var hit in rc)
             {
                 if (hit.collider.TryGetComponent<AgarthanPortal>(out var p))
                 {
@@ -26,34 +25,34 @@ namespace AgarthaLib.Goodies.Portals
                     var dist = Mathf.Max(distance - Vector3.Distance(hit.collider.transform.position, position), 0);
 
                     if (RaycastPortalAware(pos, dir, dist, out var newHits, mask))
-                        hits.AddRange(newHits);
+                        result.AddRange(newHits);
 
                     break;
                 }
 
-                hits.Add(hit);
+                result.Add(hit);
             }
 
-            return true;
+            return result.Count > 0;
         }
 
         /// <summary>
-        ///     A <see cref="UnityEngine.Physics.OverlapSphere(Vector3, float, int)"/> variation
+        ///     A <see cref="Physics.OverlapSphere(Vector3, float, int)"/> variation
         ///     that outputs objects not valid to the `overlapCondition`.
         /// </summary>
         public static bool OverlapSphereUnoccluded(Vector3 position, float radius,
-            out Collider[] hits, Predicate<Collider> overlapCondition, int mask = -1)
+            out List<Collider> hits, Predicate<Collider> overlapCondition, int mask = -1)
         {
-            hits = new Collider[0];
+            hits = new();
 
-            var sc = UnityEngine.Physics.OverlapSphere(position, radius, mask);
+            var sc = Physics.OverlapSphere(position, radius, mask);
             if (sc.Length == 0) return false;
 
             var cache = new List<Collider>();
             foreach (var hit in sc)
             {
                 var direction = hit.transform.position - position;
-                var rc = UnityEngine.Physics.RaycastAll(position, direction.normalized, radius, mask);
+                var rc = Physics.RaycastAll(position, direction.normalized, radius, mask);
                 foreach (var rhit in rc)
                 {
                     if (overlapCondition.Invoke(rhit.collider))
@@ -63,20 +62,21 @@ namespace AgarthaLib.Goodies.Portals
                 }
             }
 
-            hits = cache.Distinct().ToArray();
+            hits = cache.Distinct().ToList();
             return true;
         }
 
         public static bool OverlapHalfSphereUnoccluded(Vector3 position, Vector3 forward, float radius,
-            out Collider[] hits, Predicate<Collider> overlapCondition, int mask = -1)
+            out List<Collider> hits, Predicate<Collider> overlapCondition, int mask = -1)
         {
-            hits = new Collider[0];
+            hits = new();
             if (!OverlapSphereUnoccluded(position, radius, out hits, overlapCondition, mask))
                 return false;
 
             // if Vector3.Dot() <= 0 -> object is behind the "half sphere" thus invalid.
             hits = hits.Where(q => Vector3.Dot((q.transform.position - position).normalized, forward) > 0)
-                .Distinct().ToArray();
+                .Distinct().ToList();
+
             return true;
         }
     }
