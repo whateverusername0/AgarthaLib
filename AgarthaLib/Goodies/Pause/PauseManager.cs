3609326fb937dfcc4f3d9c6de29554d7d37e34c3
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-05-02 03:05:01 +1000

    paused behavior

---
+++ AgarthaLib/Goodies/Pause/PauseManager.cs
@@ -0,0 +1,67 @@
+﻿using AgarthaLib.MonoBehavior;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace AgarthaLib.Goodies.Pause
+{
+    public class PauseManager : AgarthanSingleton<PauseManager>
+    {
+        [Header("Properties")]
+        public bool Paused = false;
+        public bool CanPause = true;
+        public bool CanUnpause = true;
+
+        [Header("Conditionals")]
+        public bool PauseOnFocus = true;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            TogglePause(false, forced: true);
+
+            CanPause = true;
+            CanUnpause = true;
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            if (Paused) { Pause(); return; }
+            else Unpause();
+
+            if (PauseOnFocus)
+                TogglePause(!Application.isFocused);
+        }
+
+        public bool TogglePause()
+            => TogglePause(!Paused);
+
+        public bool TogglePause(bool paused, bool forced = false)
+        {
+            if (paused && (CanPause || forced))
+            {
+                Paused = true;
+                return true;
+            }
+            else if (!paused && (CanUnpause || forced))
+            {
+                Paused = false;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Pause()
+        {
+            Time.timeScale = 0f;
+        }
+
+        public void Unpause()
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
---
+++ AgarthaLib/Goodies/Pause/PausedBehavior.cs
@@ -0,0 +1,45 @@
+﻿using AgarthaLib.MonoBehavior;
+using UnityEngine;
+
+namespace AgarthaLib.Goodies.Pause
+{
+    public abstract class PausedBehavior : AgarthanBehaviour
+    {
+        protected PauseManager _pause => PauseManager.Instance;
+
+        [Header("Paused Behavior")]
+        public bool UpdateOnPause = false;
+
+        protected override void Update()
+        {
+            base.Update();
+
+            if (_pause.Paused && !UpdateOnPause)
+                return;
+
+            UnpausedUpdate();
+        }
+
+        protected virtual void UnpausedUpdate() { }
+
+        protected virtual void FixedUpdate()
+        {
+            if (_pause.Paused && !UpdateOnPause)
+                return;
+
+            UnpausedFixedUpdate();
+        }
+
+        protected virtual void UnpausedFixedUpdate() { }
+
+        protected virtual void LateUpdate()
+        {
+            if (_pause.Paused && !UpdateOnPause)
+                return;
+
+            UnpausedLateUpdate();
+        }
+
+        protected virtual void UnpausedLateUpdate() { }
+    }
+}
