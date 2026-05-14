e7a6aa33f7a89485459f0a5a6cea2ade8521d242
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-05-15 05:10:37 +1000

    add navmesh agent helper

---
+++ AgarthaLib/HTN/NavMeshAgentHelper.cs
@@ -0,0 +1,54 @@
+﻿using AgarthaLib.Attributes;
+using AgarthaLib.MonoBehavior;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace AgarthaLib.HTN
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class NavMeshAgentHelper : AgarthanBehaviour
+    {
+        [ValidateNull] public NavMeshAgent Agent;
+
+        [SerializeField, EditorReadOnly] private bool _hasPath = false;
+        [SerializeField, EditorReadOnly] private NavMeshPathStatus _pathStatus;
+
+        public bool HasPath => _hasPath;
+        public NavMeshPathStatus PathStatus => _pathStatus;
+
+        [Header("Editor")]
+        [SerializeField] private bool _drawPath = true;
+        [SerializeField] private Color _pathColor = Color.yellow;
+        [SerializeField] private Color _nextPointColor = Color.red;
+
+        protected override void Update()
+        {
+            base.Update();
+
+            _hasPath = Agent.hasPath;
+            _pathStatus = Agent.pathStatus;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (!_drawPath || Agent.path == null)
+                return;
+
+            Gizmos.color = _pathColor;
+            var tr = Agent.transform;
+            var corners = Agent.path.corners;
+            for (int i = 0; i < corners.Length; i++)
+            {
+                var j = i + 1;
+                if (j >= corners.Length - 1)
+                    break;
+
+                var apos = i == 0 ? tr.position : corners[i];
+                Gizmos.DrawLine(apos, corners[j]);
+            }
+
+            Gizmos.color = _nextPointColor;
+            Gizmos.DrawLine(tr.position, Agent.nextPosition);
+        }
+    }
+}
