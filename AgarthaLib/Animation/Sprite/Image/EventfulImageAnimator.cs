3fe803fd38a9b5bed8f5a8ce1dfba72c157fb98f
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-05-20 04:29:31 +1000

    add image frame animator

---
+++ AgarthaLib/Animation/Sprite/Image/EventfulImageAnimator.cs
@@ -0,0 +1,11 @@
+﻿namespace AgarthaLib.Animation.Sprite.Image
+{
+    public class EventfulImageAnimator
+        : EventfulFrameAnimator<SpriteAnimation, EventfulSpriteAnimationContainer, UnityEngine.Sprite>
+    {
+        public UnityEngine.UI.Image Renderer;
+
+        protected override void SetFrame(UnityEngine.Sprite frame)
+            => Renderer.sprite = frame;
+    }
+}
