f69271883b4cc5c30e5e49451b8e7a934e73ec03
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-05-20 16:40:30 +1000

    cull audiosystem

--- AgarthaLib/Goodies/Audio/AudioSystem.cs
+++ AgarthaLib/Goodies/Audio/AudioSystem.cs
@@ -1,42 +1,22 @@
 ﻿using AgarthaLib.MonoBehavior;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace AgarthaLib.Goodies.Audio
 {
     public class AudioSystem : AgarthanSingleton<AudioSystem>
     {
-        public List<AudioMixerEntry> Groups;
+        public void SetGlobalVolume(float volume)
+            => AudioListener.volume = volume;
 
-        public AudioSource PlayPvs(AudioClip clip, Vector3 position, AudioMixerGroupEnum group = AudioMixerGroupEnum.Master)
+        public AudioSource PlayClip(AudioClip clip)
         {
             var go = new GameObject(clip.name);
-            go.transform.position = position;
-
-            var groupEntry = Groups.Where(q => q.Type == group).FirstOrDefault();
             var auds = go.AddComponent<AudioSource>();
             auds.clip = clip;
-            auds.outputAudioMixerGroup = groupEntry != null ? groupEntry.Data.Group : null;
-            auds.volume = groupEntry != null ? groupEntry.Data.Volume : 1f;
             auds.Play();
 
             Destroy(go, clip.length * ((Time.timeScale < 0.01f) ? 0.01f : Time.timeScale));
             return auds;
         }
-    }
-
-    [Serializable] public class AudioMixerEntry
-    {
-        public AudioMixerGroupEnum Type;
-        public AudioMixerData Data;
-    }
-
-    [Serializable] public enum AudioMixerGroupEnum
-    {
-        Master,
-        Music,
-        Sound
     }
 }
