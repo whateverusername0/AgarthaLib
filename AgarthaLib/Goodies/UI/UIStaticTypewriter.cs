e9dda2604e98f48d9bcd255656b942f098e7a210
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-05-20 16:39:59 +1000

    fix ui typewriter

--- AgarthaLib/Goodies/UI/UIStaticTypewriter.cs
+++ AgarthaLib/Goodies/UI/UIStaticTypewriter.cs
@@ -1,17 +1,25 @@
 ﻿using AgarthaLib.Attributes;
+using AgarthaLib.Extensions;
+using AgarthaLib.Goodies.Audio;
 using AgarthaLib.MonoBehavior;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace AgarthaLib.Goodies.UI
 {
     public class UIStaticTypewriter : AgarthanBehaviour
     {
+        private AudioSystem _aud => AudioSystem.Instance;
+
         [ValidateNull] public TMP_Text Text;
         [Multiline] public string TextToWrite;
         public int CPS = 10;
         public bool StartOnEnable = false;
+        public AudioClip TypingSound;
+
+        public UnityEvent OnFinishTypewriting;
 
         private void OnEnable()
         {
@@ -24,18 +32,25 @@ namespace AgarthaLib.Goodies.UI
             if (Text == null)
                 return;
 
-            StartCoroutine(DoTypewriter(text));
+            StartCoroutine(IETypewrite(text));
         }
 
-        private IEnumerator DoTypewriter(string line)
+        public IEnumerator IETypewrite(string text)
         {
-            string textBuffer = null;
-            foreach (char c in line)
+            var textBuffer = string.Empty;
+            foreach (char c in text)
             {
                 textBuffer += c;
                 Text.text = textBuffer;
-                yield return new WaitForSeconds(1 / CPS);
+
+                if (TypingSound != null)
+                    _aud.PlayClip(TypingSound).WithPitch(Random.Range(1.25f, 1.5f)).WithVolume(.5f);
+
+                yield return new WaitForSeconds(1f / CPS);
             }
+
+            OnFinishTypewriting?.Invoke();
+            yield return null;
         }
     }
 }
