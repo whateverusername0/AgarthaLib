175cff2d3f6c47430c045b92eabc22881334333e
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-06-16 22:20:44 +1000

    add LineRendererHelper. allows you to make lines in the editor more easily

---
+++ AgarthaLib/MonoBehavior/Unity/LineRendererHelper.cs
@@ -0,0 +1,26 @@
+﻿using AgarthaLib.Attributes;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AgarthaLib.MonoBehavior.Unity
+{
+    [RequireComponent(typeof(LineRenderer)), ExecuteInEditMode]
+    public class LineRendererHelper : AgarthanBehaviour
+    {
+        [ValidateNull] public LineRenderer LineRenderer;
+
+        public List<Vector3> Points = new();
+        public bool UseLocalSpace = true;
+
+        protected override void Update()
+        {
+            base.Update();
+
+            if (LineRenderer == null)
+                return;
+
+            for (int i = 0; i < Points.Count; i++)
+                LineRenderer.SetPosition(i, Points[i] + (UseLocalSpace ? transform.position : Vector3.zero));
+        }
+    }
+}
