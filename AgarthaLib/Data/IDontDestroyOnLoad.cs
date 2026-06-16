c542c316e1f66db5d781e47b1db16cd582ae9a1b
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-06-16 22:20:15 +1000

    update AgarthanSingleton to account for dontdestroyonload

---
+++ AgarthaLib/Data/IDontDestroyOnLoad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgarthaLib.Data
+{
+    /// <summary>
+    ///     Acts as an indicator and does not bear any methods.
+    ///     Is useful for specific checks.
+    /// </summary>
+    public interface IDontDestroyOnLoad
+    {
+    }
+}
--- AgarthaLib/MonoBehavior/AgarthanSingleton.cs
+++ AgarthaLib/MonoBehavior/AgarthanSingleton.cs
@@ -1,4 +1,5 @@
-﻿using AgarthaLib.Extensions;
+﻿using AgarthaLib.Data;
+using AgarthaLib.Extensions;
 using System.Linq;
 using UnityEngine;
 
@@ -15,19 +16,39 @@ namespace AgarthaLib.MonoBehavior
         {
             get
             {
-                _instance = _instance == null ? FindObjectsByType<T>(FindObjectsInactive.Include, FindObjectsSortMode.InstanceID).FirstOrDefault() : _instance;
-                _instance = _instance == null ? new GameObject() { name = $"[Singleton] {typeof(T).Name}", hideFlags = HideFlags.DontSave }.EnsureComponent<T>() : _instance;
+                // find objects of type first
+                _instance = _instance == null
+                    ? FindObjectsByType<T>(FindObjectsInactive.Include, FindObjectsSortMode.InstanceID).FirstOrDefault()
+                    : _instance;
+
+                // automatically create an instance jic
+                _instance = _instance == null
+                    ? new GameObject() { name = $"[Singleton] {typeof(T).Name}", hideFlags = HideFlags.DontSave }.EnsureComponent<T>()
+                    : _instance;
+
                 return _instance;
             }
         }
 
         protected virtual void Awake()
         {
-            if (_instance != null && _instance != (T)this)
+            if (_instance == null)
             {
-                Destroy(_instance);
                 _instance = (T)this;
+                return;
             }
+
+            // instance exists.
+            if (_instance is IDontDestroyOnLoad)
+            {
+                // it's probably important
+                this.SafeDestroy(this);
+                return;
+            }
+
+            // override
+            this.SafeDestroy(_instance);
+            _instance = (T)this;
         }
     }
 }
