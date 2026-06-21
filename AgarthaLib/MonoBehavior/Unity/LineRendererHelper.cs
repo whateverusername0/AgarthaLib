60f6577a8e89940fe920880b40372ab691442058
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-06-21 16:43:04 +1000

    add links to linerendererhelper

--- AgarthaLib/MonoBehavior/Unity/LineRendererHelper.cs
+++ AgarthaLib/MonoBehavior/Unity/LineRendererHelper.cs
@@ -9,6 +9,9 @@ namespace AgarthaLib.MonoBehavior.Unity
     {
         [ValidateNull] public LineRenderer LineRenderer;
 
+        [Tooltip("Overrides points.")]
+        public List<Transform> Links = new();
+
         public List<Vector3> Points = new();
         public bool UseLocalSpace = true;
 
@@ -19,6 +22,24 @@ namespace AgarthaLib.MonoBehavior.Unity
             if (LineRenderer == null)
                 return;
 
+            if (Links != null && Links.Count > 0)
+                UpdateLinks();
+
+            else if (Points != null && Points.Count > 0)
+                UpdatePoints();
+        }
+
+        private void UpdateLinks()
+        {
+            foreach (var t in Links)
+            {
+                if (t == null) continue;
+                LineRenderer.SetPosition(Links.IndexOf(t), t.position);
+            }
+        }
+
+        private void UpdatePoints()
+        {
             for (int i = 0; i < Points.Count; i++)
                 LineRenderer.SetPosition(i, Points[i] + (UseLocalSpace ? transform.position : Vector3.zero));
         }
