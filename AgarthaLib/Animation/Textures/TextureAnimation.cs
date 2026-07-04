8da73dcd2b82c075c23448290c2367e88d4ae558
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-07-05 08:41:46 +1000

    more icons

--- AgarthaLib/Animation/Textures/TextureAnimation.cs
+++ AgarthaLib/Animation/Textures/TextureAnimation.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using AgarthaLib.Attributes;
+using UnityEngine;
 
 namespace AgarthaLib.Animation.Textures
 {
     [CreateAssetMenu(menuName = "AgarthaLib / Animation / Textures / Texture Animation")]
     public class TextureAnimation : FrameAnimation<Texture>
     {
-        // :V
+        [ScriptableObjectIcon] public Texture Icon
+            => Frames.Count > 0 ? Frames[0] : null;
     }
 }
