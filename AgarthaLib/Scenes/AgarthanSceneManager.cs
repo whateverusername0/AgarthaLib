61cb0a6749867b36205631b807e7b8032f040393
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-07-26 05:08:51 +1000

    scene management

---
+++ AgarthaLib/Scenes/AgarthanSceneManager.cs
@@ -0,0 +1,64 @@
+﻿using AgarthaLib.MonoBehavior;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AgarthaLib.Scenes
+{
+    public class AgarthanSceneManager : AgarthanSingleton<AgarthanSceneManager>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+
+            SceneManager.activeSceneChanged += OnActiveSceneChanged;
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            SceneManager.sceneUnloaded += OnSceneUnloaded;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            SceneManager.activeSceneChanged -= OnActiveSceneChanged;
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+            SceneManager.sceneUnloaded -= OnSceneUnloaded;
+        }
+
+        private void OnActiveSceneChanged(Scene current, Scene next)
+            => RaiseGlobalEvent(new ActiveSceneChangedGlobalEvent(current, next));
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+            => RaiseGlobalEvent(new SceneLoadedGlobalEvent(scene, mode));
+
+        private void OnSceneUnloaded(Scene scene)
+            => RaiseGlobalEvent(new SceneUnloadedGlobalEvent(scene));
+
+        public void LoadScene(Scene scene, LoadSceneMode mode = LoadSceneMode.Single)
+            => SceneManager.LoadScene(scene.name, mode);
+
+        public void LoadScene(string name, LoadSceneMode mode = LoadSceneMode.Single)
+            => SceneManager.LoadScene(name, mode);
+
+        public void RestartScene()
+            => LoadScene(SceneManager.GetActiveScene());
+
+        [Obsolete("Preferably use UnloadSceneAsync instead.")]
+        public void UnloadSceneUnsafe(Scene scene)
+            => SceneManager.UnloadScene(scene.name);
+
+        public AsyncOperation UnloadSceneAsync(Scene scene)
+            => SceneManager.UnloadSceneAsync(scene.name);
+
+        public bool TryLoadSceneFromBundle(AssetBundle bundle, int index = 0,
+            LoadSceneMode mode = LoadSceneMode.Single)
+        {
+            var scenes = bundle.GetAllScenePaths();
+            if (scenes.Length == 0 || scenes.Length < index)
+                return false;
+
+            LoadScene(scenes[index], mode);
+            return true;
+        }
+    }
+}
---
+++ AgarthaLib/Scenes/SceneEvents.cs
@@ -0,0 +1,36 @@
+﻿using AgarthaLib.EventSystem;
+using UnityEngine.SceneManagement;
+
+namespace AgarthaLib.Scenes
+{
+    public class ActiveSceneChangedGlobalEvent : EventBase
+    {
+        public Scene Current, Next;
+
+        public ActiveSceneChangedGlobalEvent(Scene current, Scene next)
+        {
+            Current = current;
+            Next = next;
+        }
+    }
+
+    public class SceneLoadedGlobalEvent : EventBase
+    {
+        public Scene Scene;
+        public LoadSceneMode Mode;
+
+        public SceneLoadedGlobalEvent(Scene scene, LoadSceneMode mode)
+        {
+            Scene = scene;
+            Mode = mode;
+        }
+    }
+
+    public class SceneUnloadedGlobalEvent : EventBase
+    {
+        public Scene Scene;
+
+        public SceneUnloadedGlobalEvent(Scene scene)
+            => Scene = scene;
+    }
+}
