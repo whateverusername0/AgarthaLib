dcdddea45817d3468eeb16e1bd5a6dc7de0aa2b8
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-08-06 08:05:04 +1000

    stuff

---
+++ AgarthaLib/MonoBehavior/Unity/TilemapDebugHelper.cs
@@ -0,0 +1,25 @@
+﻿using AgarthaLib.Extensions;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace AgarthaLib.MonoBehavior.Unity
+{
+    [RequireComponent(typeof(Tilemap))]
+    public class TilemapDebugHelper : MonoBehaviour
+    {
+        private Tilemap _tilemap;
+
+        private void Awake()
+            => _tilemap = GetComponent<Tilemap>();
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            _tilemap = _tilemap == null ? GetComponent<Tilemap>() : _tilemap;
+            foreach (var pos in _tilemap.GetAllTilesPositions())
+                Handles.Label(pos - new Vector3(.5f, .5f, .5f), $"(x:{pos.x}, y:{pos.y}, z:{pos.z})");
+        }
+#endif
+    }
+}
