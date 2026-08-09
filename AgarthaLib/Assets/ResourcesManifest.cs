1c5ccc8f655f2d40d5e1379f2dbd394dd72c7996
John Git <113782077+whateverusername0@users.noreply.github.com>
2026-08-10 07:44:48 +1000

    resources manifest is real

---
+++ AgarthaLib/Assets/ResourcesManifest.cs
@@ -0,0 +1,55 @@
+﻿#if USING_NEWTONSOFT_JSON
+using AgarthaLib.Data.Serialization.SerializedTypes;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AgarthaLib.Assets
+{
+    public class ResourcesManifest : ScriptableObject, IAssetManifest
+    {
+        [SerializeField] public SerializedDictionary<string, Object> Manifest = new();
+
+        public string Serialize()
+            => JsonConvert.SerializeObject(Manifest.Keys);
+
+        public T GetAsset<T>(string path) where T : Object
+        {
+            var value = Manifest[path];
+            if (value == null) value = Resources.Load<T>(path);
+            return value as T;
+        }
+
+        public bool TryGetAsset<T>(string path, out T obj) where T : Object
+        {
+            obj = GetAsset<T>(path);
+            return obj != null;
+        }
+
+        public static ResourcesManifest LoadFromText(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return null;
+
+            var paths = JsonConvert.DeserializeObject<List<string>>(text);
+            if (paths == null || paths.Count == 0) return null;
+
+            var inst = CreateInstance<ResourcesManifest>();
+            inst.Manifest = new(GetManifestRaw(paths));
+            return inst;
+        }
+
+        public static Dictionary<string, Object> GetManifestRaw(List<string> paths)
+        {
+            var manifest = new Dictionary<string, Object>();
+            foreach (var path in paths)
+            {
+                var resource = Resources.Load(path);
+                if (resource == null) continue;
+                manifest.Add(path, resource);
+            }
+            return manifest;
+        }
+    }
+}
+#endif
